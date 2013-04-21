26eecbc2caaa33a13eb390db45d4110d5db60fb9
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2013-04-21 11:06:44 -0400

    detectorproperties linked to ccd_detector on gmosmosrawfitsfile

--- geminiutil/gmos/gmos_alchemy.py
+++ geminiutil/gmos/gmos_alchemy.py
@@ -40,51 +40,13 @@ class GMOSMask(Base):
         self.name = name
         self.program_id = program_id
 
-
-class GMOSMOSRawFITS(Base):
-    __tablename__ = 'gmos_mos_raw_fits'
-
-
-    id = Column(Integer, ForeignKey('fits_file.id'), primary_key=True)
-    date_obs = Column(DateTime)
-    instrument_id = Column(Integer, ForeignKey('instrument.id'))
-    observation_block_id = Column(Integer, ForeignKey('observation_block.id'))
-    observation_class_id = Column(Integer, ForeignKey('observation_class.id'))
-    observation_type_id = Column(Integer, ForeignKey('observation_type.id'))
-    object_id = Column(Integer, ForeignKey('object.id'))
-    mask_id = Column(Integer, ForeignKey('gmos_mask.id'))
-    exclude = Column(Boolean)
-
-
-    fits = relationship(FITSFile, uselist=False, backref='raw_fits')
-    instrument = relationship(Instrument, uselist=False, backref='raw_fits')
-    observation_block = relationship(ObservationBlock, uselist=False, backref='raw_fits')
-    observation_class = relationship(ObservationClass, uselist=False, backref='raw_fits')
-    observation_type = relationship(ObservationType, uselist=False, backref='raw_fits')
-    object = relationship(base.Object, uselist=False, backref='raw_fits')
-    mask = relationship(GMOSMask, uselist=False, backref='raw_fits')
-
-    def __init__(self, date_obs, instrument_id, observation_block_id, observation_class_id, observation_type_id, object_id, mask_id=None, exclude=False):
-        self.date_obs = date_obs
-        self.instrument_id = instrument_id
-        self.observation_block_id = observation_block_id
-        self.observation_class_id = observation_class_id
-        self.observation_type_id = observation_type_id
-        self.exclude = exclude
-
-        self.mask_id = mask_id
-        self.object_id = object_id
-
-    def __repr__(self):
-        return '<gmos fits="%s" class="%s" type="%s" object="%s">' % (self.fits.fname, self.observation_class.name,
-                                                                  self.observation_type.name, self.object.name)
-
-
 class GMOSDetectorProperties(Base):
     __tablename__ = 'gmos_detector_properties'
+
     """
     Detector properties table
     """
+
     id = Column(Integer, primary_key=True)
     naxis1 = Column(Integer)
     naxis2 = Column(Integer)
@@ -112,7 +74,6 @@ class GMOSDetectorProperties(Base):
                                   (func.abs(cls.read_noise - header['RDNOISE']) / header['RDNOISE']) < 0.0001,
                                   cls.x_binning==x_binning, cls.y_binning==y_binning,
                                   cls.frame_id==int(header['FRAMEID'])).all()
-        print "hello", detector_object
         if detector_object == []:
             detector_object = cls(header['NAXIS1'], header['NAXIS2'], header['CCDNAME'], readout_direction, header['GAIN'],
                        header['RDNOISE'], x_binning, y_binning, header['FRAMEID'])
@@ -138,3 +99,54 @@ class GMOSDetectorProperties(Base):
     def __repr__(self):
         return "<detector id=%d ccdname=%s xbin=%d ybin=%d gain=%.2f>" % (self.id, self.ccd_name, self.x_binning,
         self.y_binning, self.gain)
+
+
+class GMOSMOSRawFITS(Base):
+    __tablename__ = 'gmos_mos_raw_fits'
+
+
+    id = Column(Integer, ForeignKey('fits_file.id'), primary_key=True)
+    date_obs = Column(DateTime)
+    instrument_id = Column(Integer, ForeignKey('instrument.id'))
+    observation_block_id = Column(Integer, ForeignKey('observation_block.id'))
+    observation_class_id = Column(Integer, ForeignKey('observation_class.id'))
+    observation_type_id = Column(Integer, ForeignKey('observation_type.id'))
+    object_id = Column(Integer, ForeignKey('object.id'))
+    mask_id = Column(Integer, ForeignKey('gmos_mask.id'))
+    chip1_detector_id = Column(Integer, ForeignKey('gmos_detector_properties.id'))
+    chip2_detector_id = Column(Integer, ForeignKey('gmos_detector_properties.id'))
+    chip3_detector_id = Column(Integer, ForeignKey('gmos_detector_properties.id'))
+
+    exclude = Column(Boolean)
+
+
+    fits = relationship(FITSFile, uselist=False, backref='raw_fits')
+    instrument = relationship(Instrument, uselist=False, backref='raw_fits')
+    observation_block = relationship(ObservationBlock, uselist=False, backref='raw_fits')
+    observation_class = relationship(ObservationClass, uselist=False, backref='raw_fits')
+    observation_type = relationship(ObservationType, uselist=False, backref='raw_fits')
+    object = relationship(base.Object, uselist=False, backref='raw_fits')
+    mask = relationship(GMOSMask, uselist=False, backref='raw_fits')
+    chip1_detector = relationship(GMOSDetectorProperties, primaryjoin=(GMOSDetectorProperties.id==chip1_detector_id),
+                                uselist=False, backref='raw_fits')
+
+    chip2_detector = relationship(GMOSDetectorProperties, primaryjoin=(GMOSDetectorProperties.id==chip2_detector_id),
+                                uselist=False, backref='raw_fits')
+
+    chip3_detector = relationship(GMOSDetectorProperties, primaryjoin=(GMOSDetectorProperties.id==chip3_detector_id),
+                                uselist=False, backref='raw_fits')
+
+    def __init__(self, date_obs, instrument_id, observation_block_id, observation_class_id, observation_type_id, object_id, mask_id=None, exclude=False):
+        self.date_obs = date_obs
+        self.instrument_id = instrument_id
+        self.observation_block_id = observation_block_id
+        self.observation_class_id = observation_class_id
+        self.observation_type_id = observation_type_id
+        self.exclude = exclude
+
+        self.mask_id = mask_id
+        self.object_id = object_id
+
+    def __repr__(self):
+        return '<gmos fits="%s" class="%s" type="%s" object="%s">' % (self.fits.fname, self.observation_class.name,
+                                                                  self.observation_type.name, self.object.name)
