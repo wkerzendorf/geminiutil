1fe70412f6b8e78b268926d2b0fcce5e14561bd7
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2013-05-31 21:54:30 -0400

    fixed the GMOSPrepare and split off the slit cutting

--- geminiutil/gmos/__init__.py
+++ geminiutil/gmos/__init__.py
@@ -4,7 +4,7 @@ from .gmos_alchemy import GMOSMOSRawFITS, GMOSDetector, GMOSFilter, GMOSGrating,
 from . import gmos_reduction as reduction
 
 from .gmos_util import DetectorGeometry
-from .gmos_basics import GMOSPrepare, GMOSCCDImage
+from .gmos_basics import GMOSPrepare, GMOSMOSCutSlits
 
 #from geminiutil.gmos.basic.prepare import GMOSCCDImage
 
--- geminiutil/gmos/gmos_basics.py
+++ geminiutil/gmos/gmos_basics.py
@@ -86,55 +86,98 @@ class GMOSPrepare(object): # will be base when we know what
             #####
 
             amplifier_data = prepare.correct_gain(amplifier_data, gain=detector.gain)
-            amplifier_data.name = 'chip%d' % i
+            amplifier_data.name = 'chip%d.data' % i
 
             final_hdu_list += [amplifier_data]
 
-class GMOSMOSCutSlits(Base):
+            fits.HDUList(final_hdu_list).writeto(full_path, clobber=True)
+        return FITSFile.from_fits_file(full_path)
+
+
+class GMOSMOSCutSlits(object):
         __tablename__ = 'gmosmos_cut_slits'
 
         file_prefix = 'cut'
-        #####
-        #Cutting the Mask
-        #####
-        mdf_table = gmos_raw_object.mask.fits.fits_data['MDF'].data
-        naxis1 = final_hdu_list['DATA'].header['naxis1']
-        naxis2 = final_hdu_list['DATA'].header['naxis2']
 
-        current_instrument_setup = gmos_raw_object.instrument_setup
-        x_scale = current_instrument_setup.x_scale
-        y_scale = current_instrument_setup.y_scale
+        def __init__(self, mode='header_cut'):
+            pass
 
-        wavelength_offset = current_instrument_setup.grating.wavelength_offset
-        spectral_pixel_scale = current_instrument_setup.spectral_pixel_scale
-        wavelength_start = current_instrument_setup.wavelength_start
-        wavelength_end = current_instrument_setup.wavelength_end
-        wavelength_central = current_instrument_setup.grating_central_wavelength
-        y_distortion_coefficients = current_instrument_setup.y_distortion_coefficients
-        y_offset = current_instrument_setup.y_offset
-        anamorphic_factor = current_instrument_setup.anamorphic_factor
-        arcsecpermm = current_instrument_setup.arcsecpermm
-        prepared_mdf_table = mask_cut.prepare_mdf_table(mdf_table, naxis1, naxis2, x_scale, y_scale, anamorphic_factor,
-                                                        wavelength_offset, spectral_pixel_scale, wavelength_start,
-                                                        wavelength_central, wavelength_end,
-                                                        y_distortion_coefficients=y_distortion_coefficients,
-                                                        arcsecpermm = arcsecpermm, y_offset=y_offset)
+        def __call__(self, gmos_raw_object, fname=None, destination_dir='.', write_steps=False, write_cut_image=None):
+            """
+            Preparing the Image
 
-        if write_cut_image:
-            cut_image, cut_hdu_list = mask_cut.cut_slits(final_hdu_list['DATA'].data, prepared_mdf_table,
-                                            uncertainty=final_hdu_list['UNCERTAINTY'].data,
-                                            mask=final_hdu_list['MASK'].data, return_cut_image=True)
+            Parameters
+            ----------
 
-            fits.ImageHDU(data=cut_image).writeto(os.path.join(destination_dir, write_cut_image), clobber=True)
-        else:
-            cut_hdu_list = mask_cut.cut_slits(final_hdu_list['DATA'].data, prepared_mdf_table,
-                                            uncertainty=final_hdu_list['UNCERTAINTY'].data,
-                                            mask=final_hdu_list['MASK'].data)
+            gmos_raw_object :
+
+            fname :
+                output filename
+
+            write_steps :
+                write out the individual steps to the individual fits files
+
+            write_cut_image :
+                write out the cut_image to a fits file with name specified in variable
 
 
 
+            """
 
-        cut_hdu_list.insert(0, final_hdu_list[0])
-        fits.HDUList(cut_hdu_list).writeto(full_path, clobber=True)
-        return FITSFile.from_fits_file(full_path)
+            if fname is None:
+                fname = '%s-%s' % (self.file_prefix, gmos_raw_object.fits.fname)
+
+            full_path = os.path.join(destination_dir, fname)
+
+            fits_data = gmos_raw_object.fits.fits_data
+
+            final_hdu_list = [fits_data[0].copy()]
+
+
+            #####
+            #Cutting the Mask
+            #####
+            mdf_table = gmos_raw_object.mask.fits.fits_data['MDF'].data
+            naxis1 = final_hdu_list['DATA'].header['naxis1']
+            naxis2 = final_hdu_list['DATA'].header['naxis2']
+
+            current_instrument_setup = gmos_raw_object.instrument_setup
+            x_scale = current_instrument_setup.x_scale
+            y_scale = current_instrument_setup.y_scale
+
+            wavelength_offset = current_instrument_setup.grating.wavelength_offset
+            spectral_pixel_scale = current_instrument_setup.spectral_pixel_scale
+            wavelength_start = current_instrument_setup.wavelength_start
+            wavelength_end = current_instrument_setup.wavelength_end
+            wavelength_central = current_instrument_setup.grating_central_wavelength
+            y_distortion_coefficients = current_instrument_setup.y_distortion_coefficients
+            y_offset = current_instrument_setup.y_offset
+            anamorphic_factor = current_instrument_setup.anamorphic_factor
+            arcsecpermm = current_instrument_setup.arcsecpermm
+            prepared_mdf_table = mask_cut.prepare_mdf_table(mdf_table, naxis1, naxis2, x_scale, y_scale, anamorphic_factor,
+                                                            wavelength_offset, spectral_pixel_scale, wavelength_start,
+                                                            wavelength_central, wavelength_end,
+                                                            y_distortion_coefficients=y_distortion_coefficients,
+                                                            arcsecpermm = arcsecpermm, y_offset=y_offset)
+
+
+            return prepared_mdf_table
+
+            if write_cut_image:
+                cut_image, cut_hdu_list = mask_cut.cut_slits(final_hdu_list['DATA'].data, prepared_mdf_table,
+                                                uncertainty=final_hdu_list['UNCERTAINTY'].data,
+                                                mask=final_hdu_list['MASK'].data, return_cut_image=True)
+
+                fits.ImageHDU(data=cut_image).writeto(os.path.join(destination_dir, write_cut_image), clobber=True)
+            else:
+                cut_hdu_list = mask_cut.cut_slits(final_hdu_list['DATA'].data, prepared_mdf_table,
+                                                uncertainty=final_hdu_list['UNCERTAINTY'].data,
+                                                mask=final_hdu_list['MASK'].data)
+
+
+
+
+            cut_hdu_list.insert(0, final_hdu_list[0])
+            fits.HDUList(cut_hdu_list).writeto(full_path, clobber=True)
+            return FITSFile.from_fits_file(full_path)
 
